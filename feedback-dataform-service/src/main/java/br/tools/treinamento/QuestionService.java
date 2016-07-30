ec59ef191d3e77f5d1ec66d8d10ef64d596eddd0
Alex S. Rosa <alex.rosa@tools.com.br>
2016-07-30 16:13:53 -0300

    TeamFinal commitando :D

---
+++ feedback-dataform-service/src/main/java/br/tools/treinamento/QuestionService.java
@@ -0,0 +1,32 @@
+package br.tools.treinamento;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+ 
+@Stateless
+public class QuestionService implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void load (List<Question> questions){
+
+		for (Question question : questions) {
+			entityManager.persist(question);
+		}
+		
+		
+		
+		
+		
+	}
+}
