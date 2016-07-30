96603ab67dd06dd109baab2ad53879940818b5be
wschneider <william.schneider@tools.com.br>
2016-07-30 15:52:05 -0300

    Configuração do método save

--- feedback-dataform-service/src/main/java/br/tools/treinamento/FormService.java
+++ feedback-dataform-service/src/main/java/br/tools/treinamento/FormService.java
@@ -1,8 +1,19 @@
 package br.tools.treinamento;
 
-public class FormService {
+import java.io.Serializable;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class FormService implements Serializable{
 	
-	public Form save(Form form){
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public Form save(Form form){		
+		entityManager.merge(form);
 		return form;
 		
 	}
