a206dd497aa0b7e326b0b3a460c82aff259dffd4
Alex S. Rosa <alex.rosa@tools.com.br>
2016-08-27 18:09:59 -0300

    Inclusão rest

--- feedback-dataform-service/src/main/java/br/tools/treinamento/QuestionService.java
+++ feedback-dataform-service/src/main/java/br/tools/treinamento/QuestionService.java
@@ -7,21 +7,44 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import br.tools.treinamento.bean.Question;
+
 @Stateless
 public class QuestionService implements Serializable {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    @PersistenceContext
-    private EntityManager entityManager;
+	@PersistenceContext
+	private EntityManager entityManager;
 
-    public void load(final List<Question> questions) {
+	public void load(final List<br.tools.treinamento.Question> questions) {
 
-        for (final Question question : questions) {
-            entityManager.persist(question);
-        }
-    }
+		for (br.tools.treinamento.Question question : questions) {
+			entityManager.persist(question);
+		}
+	}
+
+	public List<Question> searchAll() {
+		return entityManager.createQuery(" SELECT q FROM Question q", Question.class).getResultList();
+	}
+
+	public Object findById(Long id) {
+		return entityManager.find(Question.class, id);
+	}
+
+	public void delete(Long id) {
+		entityManager.remove(entityManager.find(Question.class, id));
+	}
+
+	public Question create(Question question) {
+		entityManager.persist(question);
+		return question;
+	}
+
+	public Question update(Question question) {
+		return entityManager.merge(entityManager.find(Question.class, question.getId()));
+	}
 }
---
+++ feedback-dataform-service/src/main/java/br/tools/treinamento/rest/QuestionRest.java
@@ -0,0 +1,111 @@
+package br.tools.treinamento.rest;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import br.tools.treinamento.QuestionService;
+import br.tools.treinamento.bean.Question;
+
+@Stateless
+@Path("/question")
+public class QuestionRest implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7371352048092236794L;
+
+	@Path("/question")
+	@GET 
+	@Produces("application/json")
+	public Response search() {
+		QuestionService questionService = new QuestionService();	
+		
+		final List<br.tools.treinamento.Question> questions = new ArrayList<br.tools.treinamento.Question>();
+		
+		for ( Question questionBean :  questionService.searchAll()) {
+			br.tools.treinamento.Question questionEntity = new br.tools.treinamento.Question();
+			questionEntity.setId(questionBean.getId());
+			questionEntity.setFieldId(questionBean.getFieldId());
+			questionEntity.setQuestion(questionBean.getQuestion());
+			questions.add(questionEntity);
+		}
+
+		return Response.ok(questions).build();
+	}
+	
+    @Path("/question/{id}") 
+    @GET 
+    @Produces("application/json") 
+    public Response find(@PathParam("id") Long id) { 
+		QuestionService questionService = new QuestionService();    
+		
+		Question questionBean = (Question) questionService.findById(id);
+		br.tools.treinamento.Question questionEntity = new br.tools.treinamento.Question();
+		
+		questionEntity.setId(questionBean.getId());
+		questionEntity.setFieldId(questionBean.getFieldId());
+		questionEntity.setQuestion(questionBean.getQuestion());
+		
+        return Response.ok(questionEntity).build();
+    }
+	
+    @Path("/{id}")
+    @DELETE 
+    @Produces("application/json") 
+    @Consumes("application/json") 
+    public Response delete(@PathParam("id") Long id) {
+		QuestionService questionService = new QuestionService();     
+		questionService.delete(id);
+		
+        return Response.ok().build();
+    } 
+    
+    @Path("/")
+    @POST 
+    @Produces("application/json") 
+    @Consumes("application/json") 
+    public Response create(Question question) {
+		QuestionService questionService = new QuestionService();    
+		
+		Question questionBean = (Question) questionService.create(question);
+		br.tools.treinamento.Question questionEntity = new br.tools.treinamento.Question();
+		
+		questionEntity.setId(questionBean.getId());
+		questionEntity.setFieldId(questionBean.getFieldId());
+		questionEntity.setQuestion(questionBean.getQuestion());		
+		
+        return Response.ok(questionEntity).build();
+    }
+
+    @Path("/")
+    @PUT 
+    @Produces("application/json") 
+    @Consumes("application/json") 
+    public Response update(Question question) {
+		QuestionService questionService = new QuestionService();     
+
+		Question questionBean = (Question) questionService.update(question);
+		br.tools.treinamento.Question questionEntity = new br.tools.treinamento.Question();
+		
+		questionEntity.setId(questionBean.getId());
+		questionEntity.setFieldId(questionBean.getFieldId());
+		questionEntity.setQuestion(questionBean.getQuestion());	
+		
+        return Response.ok(questionEntity).build();
+    } 
+	
+	
+}
